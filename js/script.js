d6b68b697908f8bc60fe92764809606c7744af6c
bradfordharrison2606 <morryspence245@gmail.com>
2024-06-21 12:18:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     posts.forEach(post => {
         fetch(`posts/${post}`)
-            .then(response => response.text())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
             .then(data => {
                 const postTitle = data.split('\n')[0].replace('# ', '');
                 const postElement = document.createElement('div');
                 postElement.innerHTML = `<h2><a href="post.html?post=${post}">${postTitle}</a></h2>`;
                 postsContainer.appendChild(postElement);
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
             });
     });
 });
